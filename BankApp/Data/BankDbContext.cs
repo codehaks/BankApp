87c654b8c5f142a002e3278d87550e35270e4aa7
Hakim Ghods <ghods.hakim@hotmail.com>
2018-06-08 17:26:56 +0430

    Add project files.

---
+++ BankApp/Data/BankDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankApp.Data
+{
+    public class BankDbContext : DbContext
+    {
+        public BankDbContext(DbContextOptions<BankDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ BankApp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using BankApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IList<User> Users { get; set; }
+
+        public void OnGet([FromServices] BankDbContext _db)
+        {
+            Users = _db.Users.Take(100).ToList();
+        }
+    }
+}
