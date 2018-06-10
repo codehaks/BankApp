177ff6eb22728a4a49a31ff53da31ea899aab931
Hakim Ghods <ghods.hakim@hotmail.com>
2018-06-10 23:52:30 +0430

    Tag helper

---
+++ BankApp/Pages/Test.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using BankApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankApp.Pages
+{
+    public class TestModel : PageModel
+    {
+        public IList<User> Users { get; set; }
+
+        public const int PageSize = 10;
+
+        public int TotalPages { get; set; }
+
+        [ViewData]
+        public int PageNumber { get; set; }
+
+        public void OnGet([FromServices] BankDbContext _db, int number = 1)
+        {
+            Expression<Func<User, User>> expSelect = u => new User { Givenname = u.Givenname, Maidenname = u.Maidenname };
+
+            var skip = PageSize * (number - 1);
+
+            TotalPages = _db.Users.Count() / PageSize;
+
+            Users = _db.Users
+                .Select(expSelect)
+                .Skip(skip)
+                .Take(PageSize)
+                .ToList();
+
+            PageNumber = number;
+        }
+    }
+}
