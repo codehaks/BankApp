1f91ad12c058f0008387632d49eaa5fc9140e3b4
Hakim Ghods <ghods.hakim@hotmail.com>
2018-06-10 23:17:11 +0430

    Pagination

--- BankApp/Pages/Index.cshtml.cs
+++ BankApp/Pages/Index.cshtml.cs
@@ -12,23 +12,28 @@ namespace BankApp.Pages
     {
         public IList<User> Users { get; set; }
 
-        public void OnGet([FromServices] BankDbContext _db)
-        {
-            //Func<User, User> funcSelect = u => new User { Givenname = u.Givenname, Maidenname = u.Maidenname };
+        public const int PageSize = 10;
 
-            //Users = _db.Users
-            //  .Select(funcSelect)
-            //  .Take(10).ToList();
+        public int TotalPages { get; set; }
 
-            //Expression ex;
-            //ex = () => 10;
+        [ViewData]
+        public int PageNumber { get; set; }
 
+        public void OnGet([FromServices] BankDbContext _db, int number = 1)
+        {            
+            Expression<Func<User, User>> expSelect = u => new User { Givenname = u.Givenname, Maidenname = u.Maidenname };
 
-            Expression<Func<User, User>> expSelect =  u => new User { Givenname = u.Givenname, Maidenname = u.Maidenname } ;   
+            var skip = PageSize * (number - 1);
+
+            TotalPages = _db.Users.Count()/ PageSize;
 
             Users = _db.Users
-                .Select(expSelect)                
-                .Take(10).ToList();
+                .Select(expSelect)
+                .Skip(skip)
+                .Take(PageSize)
+                .ToList();
+
+            PageNumber = number;
         }
     }
 }
